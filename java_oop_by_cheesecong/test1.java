1b14b58a2afef2da6f664ee8d4cf29f7f3ac9b9d
Cong Chi <tacongchi071006@gmail.com>
2025-09-05 18:29:53 +0700

    commit

--- java_oop_by_cheesecong/test1.java
+++ java_oop_by_cheesecong/test1.java
@@ -2,5 +2,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("xin chao");
         System.out.println("chi thoa");
+        System.out.println("chi thoa");
     }
 }
